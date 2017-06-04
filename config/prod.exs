f47aaf16e137f904fc40e5563ecf505a7ae672db
Raincal <cyj94228@gmail.com>
2017-06-05 05:29:23 +0800

    fix config

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :phx_blog, PhxBlog.Web.Endpoint,
   on_init: {PhxBlog.Web.Endpoint, :load_from_system_env, []},
   url: [host: "peaceful-ridge-14452.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/asset-manifest.json"
+  cache_static_manifest: "priv/static/asset-manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
